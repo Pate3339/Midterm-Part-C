6fbcc05905e47de7bb76dd78e9bb37401c2b977a
HP <HP@10.0.0.132>
2022-02-23 11:06:56 -0500

    Object Added

---
+++ src/TestUserDesign.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author HP
+ */
+public class TestUserDesign {
+    public static void main(String args[]){
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Entr your name");
+        String name = scn.nextLine();
+        System.out.println("Choose one Gerne number");
+        int gen = scn.nextInt();
+       
+    }
+}
